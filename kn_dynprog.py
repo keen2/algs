75893392bbb885043b16555ccbb3838ac2e3b37e
Andrei Ermishin <andrey.yermishin@gmail.com>
2020-03-10 13:12:28 +0300

    Add info

--- kn_dynprog.py
+++ kn_dynprog.py
@@ -1,3 +1,9 @@
+__author__ = "Andrei Ermishin"
+__copyright__ = "Copyright (c) 2019"
+__license__ = "GNU GPLv3"
+__email__ = "andrey.yermishin@gmail.com"
+
+
 def dp(capacity, weights):
     """
     O(n * 2**log(capacity)).
